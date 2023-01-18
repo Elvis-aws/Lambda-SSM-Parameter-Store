3b3e3c94d8cf3a30ccb4b0e227d57c4f59ed9853
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2023-01-18 05:50:12 +0000

    updates

--- Lambda/async-and-sync.r
+++ Lambda/async-and-sync.r
@@ -1,12 +1,10 @@
 
 
-
-
 ***********************
 Synchronous invocation
 ***********************
     - No error handling
-    - Each function is involked after the other 
+    - Each function is invoked after the other
     - We need to wait for the results before we proceed to the next function 
     - Your application has to handle any retries due to failures
     - 
@@ -14,7 +12,7 @@ Synchronous invocation
 ***********************
 Asynchronous invocation
 ***********************
-    - Asynchronous invocation implies function is invocked but we do not know the results
+    - Asynchronous invocation implies function is invoked but we do not know the results
         - S3, SNS,SQS, Lambda, CloudWatch Events
         - The events are placed in event queues
         - lambda attempts to re-try
@@ -35,7 +33,7 @@ Asynchronous invocation
         - Duplicate log entries will be seen in cloudwatch log incase of retries
         - DLQ will be needed to process the failed events.
             - When we invoke a function asynchronously, we do not get the results but just the status code that 
-              the functionwas successfully invoked
-            - We thus need a DLQ to capture any failed invokations 
+              the function was successfully invoked
+            - We thus need a DLQ to capture any failed invocations
             - We can always configure a number of retries before sending to our DLQ in SQS
     
